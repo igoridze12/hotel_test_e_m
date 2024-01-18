c32c7458fa3ba68b19da44b677a7a84522709e8f
Igor Grushin <igor73rus12@gmail.com>
2024-01-18 19:17:00 +0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# hotel_test_e_m
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/hotel_test_e_m/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hotel_test_e_m
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
